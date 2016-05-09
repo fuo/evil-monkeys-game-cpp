0002a91f6eec0a5b776f908dca523bc35ecfcfa7
fuo <pha.m@icloud.com>
2016-05-09 23:05:06 +0700

    Initial Commit

---
+++ evil-monkeys/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  evil-monkeys
+//
+//  Created by phuong on 5/9/16.
+//  Copyright © 2016 badila. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
