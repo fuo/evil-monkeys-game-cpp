0747ca4afe37a3a4fdf2c4b0f2a444bbf3e948b3
fuo <pha.m@icloud.com>
2016-05-22 17:30:11 +0700

    add the Fireball as a moving Bomb (extend the Bomb Sprite)

---
+++ header/fireball.h
@@ -0,0 +1,28 @@
+//
+//  fireball.h
+//  Evil Monkey Game
+//
+//  Created by phuong on 1/28/16.
+//  Copyright © 2016 phuong. All rights reserved.
+//
+
+#ifndef fireball_h
+#define fireball_h
+
+#include "bomb.h"
+
+namespace EvilMonkeys
+{
+    class Fireball : public Bomb
+    {
+    public:
+        Fireball(DrawEngine *de, int sprite_index, float xpos, float ypos,
+                 float xDir, float yDir, int i_lives = 1);
+     
+    private:
+        
+        void __idleUpdate(void);
+        
+    };
+}
+#endif /* fireball_h */
---
+++ source/fireball.cpp
@@ -0,0 +1,34 @@
+//
+//  fireball.cpp
+//  Evil Monkey Game
+//
+//  Created by phuong on 1/28/16.
+//  Copyright © 2016 phuong. All rights reserved.
+//
+
+#include "fireball.h"
+
+#include "level.h"
+
+using namespace EvilMonkeys;
+
+#include <list>
+
+Fireball::Fireball(DrawEngine *de, int sprite_index, float xpos, float ypos,
+                   float xDir, float yDir, int i_lives) : Bomb(de, sprite_index, xpos, ypos, i_lives)
+{
+    facingDirection.x = xDir;
+    facingDirection.y = yDir;
+    
+    setClassID(FIREBALL_CLASSID);
+}
+
+void Fireball::__idleUpdate()
+{
+    if (Sprite::__move(facingDirection.x, facingDirection.y))
+        // kill the enemy OR Fireball got hit and then kill itself
+        Bomb::__idleUpdate();
+    else
+        // hit the wall border
+        __addLives(-1);    
+}
